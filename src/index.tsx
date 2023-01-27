b255570c98a74cec686c40fa71927ea52a1f55a3
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2023-01-27 22:21:31 +0900

    style: 초기세팅

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "Router";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { theme } from "./theme";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
